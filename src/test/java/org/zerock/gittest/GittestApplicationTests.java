91b541ff7349bffaf97b5f1460a20a4e5337948d
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-10-17 13:03:22 +0900

    PHJ_git test용 프로젝트 생성

---
+++ src/test/java/org/zerock/gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
